f000c6afd80a075fbd7478aab931a4afee63d05c
Dave <dave@stackden.com>
2018-03-21 17:08:40 -0700

    Stopwatch Project Completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <App/>,
     document.getElementById('root')
 );
+
+
+
+// function greeting(user){
+//     return ( <div>
+//                 <h1 class="container">Welcome {user.name}</h1>
+//                 <h2 class="text-muted">Your lucky number is: {user.luckyNumber}</h2>
+//             </div>
+//     );
+// }
+
+// function luckyNumber(){
+//         const randomNumber = Math.floor((Math.random() * 1000) + 1)
+//         return randomNumber
+// }
+
+// const user = {
+//     name: "Dave",
+//     luckyNumber: luckyNumber()
+// }
+
+// ReactDOM.render(
+//     greeting(user),
+//     document.getElementById('root')
+// );
